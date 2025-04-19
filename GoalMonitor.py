9b5d369efd5989857049fa1b918a3e25ba9951d0
samcarri <samcarri@ucm.es>
2025-04-19 20:20:04 +0200

    Replanificacion por ticks (a lo maincra)

--- GoalMonitor.py
+++ GoalMonitor.py
@@ -14,12 +14,13 @@ class GoalMonitor:
         self.problem = problem
         self.lastTime = -1
         self.recalculate = False
-
+        self.ticks=0
     def ForceToRecalculate(self):
         self.recalculate = True
 
     #determina si necesitamos replanificar
     def NeedReplaning(self, perception, map, agent):
+        self.ticks+=1
         #Se ha detectado una bala y por lo tanto debemos actuar en consecuencia
         if perception[AgentConsts.NEIGHBORHOOD_RIGHT]==AgentConsts.SHELL or perception[AgentConsts.NEIGHBORHOOD_LEFT]==AgentConsts.SHELL or perception[AgentConsts.NEIGHBORHOOD_UP]==AgentConsts.SHELL or perception[AgentConsts.NEIGHBORHOOD_DOWN]==AgentConsts.SHELL:
             return True
@@ -27,16 +28,16 @@ class GoalMonitor:
             return self.goals[self.GOAL_LIFE]
         elif abs(perception[AgentConsts.PLAYER_X]-perception[AgentConsts.AGENT_X])<6 and abs(perception[AgentConsts.PLAYER_Y]-perception[AgentConsts.AGENT_Y])<6:
             return True
-        
+        if(self.ticks>20): 
+            print("Replanificacion por ticks")
+            self.ticks=0
+            return True
         return False
     
     #selecciona la meta mas adecuada al estado actual
     def SelectGoal(self, perception, map, agent):
         if (perception[AgentConsts.NEIGHBORHOOD_RIGHT]==AgentConsts.SHELL or perception[AgentConsts.NEIGHBORHOOD_LEFT]==AgentConsts.SHELL or perception[AgentConsts.NEIGHBORHOOD_UP]==AgentConsts.SHELL or perception[AgentConsts.NEIGHBORHOOD_DOWN]==AgentConsts.SHELL) and perception[AgentConsts.CAN_FIRE]==AgentConsts.SHELL:
             return self.goals[GOAL_PLAYER]
-
-        #if (perception[NEIGHBORHOOD_RIGHT]==SHELL or perception[NEIGHBORHOOD_LEFT]==SHELL or perception[NEIGHBORHOOD_UP]==SHELL or perception[NEIGHBORHOOD_DOWN]==SHELL) and perception[CAN_ATTACK]!=SHELL:
-           # return self.goals[4]
 
         if abs(perception[AgentConsts.PLAYER_X]-perception[AgentConsts.AGENT_X])<6 and abs(perception[AgentConsts.PLAYER_Y]-perception[AgentConsts.AGENT_Y])<6:
             return self.goals[self.GOAL_PLAYER]
